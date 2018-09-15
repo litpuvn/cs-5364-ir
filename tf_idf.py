096fb0ab7738b113bf8428b99a490bb9b04ede49
liang112233 <36117480+liang112233@users.noreply.github.com>
2018-09-15 15:26:36 -0500

    debugprogram
    debugprogram

--- tf_idf.py
+++ tf_idf.py
@@ -5,7 +5,8 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 
 
-df = pd.read_csv('file.text', sep="\x13")
+df = pd.read_csv('tweets/filtered_data.txt', sep="\000")
+df.columns =['id', 'tweet_date', 'tweet']
 
 stemmer = SnowballStemmer("english")
 stop_words = stopwords.words('english')
@@ -17,12 +18,12 @@ df['words'] = df['tweet'].apply(lambda x: ' '.join([word for word in x.split(' '
 ## stem words
 df['stemmed'] = df['words'].apply(lambda x: ' '.join([stemmer.stem(y) for y in x.decode('utf-8').split(' ')]))
 df['stemmed'].head()
-
-
-## vectorize with tf-idf
-
-tvec = TfidfVectorizer(min_df=.0025, max_df=.1, stop_words='english', ngram_range=[1, 1])
-tvec_weights = tvec.fit_transform(df.stemmed.dropna())
-weights = np.asarray(tvec_weights.mean(axis=0)).ravel().tolist()
-weights_df = pd.DataFrame({'term': tvec.get_feature_names(), 'weight': weights})
-weights_df.sort_values(by='weight', ascending=False).head(20)
+#
+#
+# ## vectorize with tf-idf
+#
+# tvec = TfidfVectorizer(min_df=.0025, max_df=.1, stop_words='english', ngram_range=[1, 1])
+# tvec_weights = tvec.fit_transform(df.stemmed.dropna())
+# weights = np.asarray(tvec_weights.mean(axis=0)).ravel().tolist()
+# weights_df = pd.DataFrame({'term': tvec.get_feature_names(), 'weight': weights})
+# weights_df.sort_values(by='weight', ascending=False).head(20)
