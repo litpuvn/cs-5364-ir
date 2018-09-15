50d104bae6c088a34e497a7ebe61ff0a56505f6d
liang112233 <36117480+liang112233@users.noreply.github.com>
2018-09-15 16:33:54 -0500

    term frequency
    term frequency

--- tf_idf.py
+++ tf_idf.py
@@ -1,12 +1,21 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import pandas as pd
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 
 
-df = pd.read_csv('tweets/clean_tweets.csv', sep=',', encoding="utf-8")
-df.columns =['id','tweet']
+df = pd.read_csv('tweets/clean_tweets.csv', sep=',')
+df = df.dropna()
+
+## vectorize with term frequency
+cvec = CountVectorizer()
+cvec_weights = cvec.fit_transform(df['tweet'])
+weights = np.asarray(cvec_weights.mean(axis=0)).ravel().tolist()
+weights_df = pd.DataFrame({'term': cvec.get_feature_names(), 'weight': weights})
+weights_df.sort_values(by='weight', ascending=False).head(20)
+print (weights_df)
 
 ## vectorize with tf-idf
 
@@ -15,5 +24,7 @@ tvec_weights = tvec.fit_transform(df['tweet'])
 weights = np.asarray(tvec_weights.mean(axis=0)).ravel().tolist()
 weights_df = pd.DataFrame({'term': tvec.get_feature_names(), 'weight': weights})
 weights_df.sort_values(by='weight', ascending=False).head(20)
+
+print (weights_df)
 
 print("done")
