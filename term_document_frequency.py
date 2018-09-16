cbbfe90cd788edb6b94dd5f49bd0274eff62424b
Hoang Long <litpuvn@gmail.com>
2018-09-15 22:11:18 -0500

    add heatmap

--- term_document_frequency.py
+++ term_document_frequency.py
@@ -1,5 +1,11 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import seaborn as sns
+
+
 
 df = pd.read_table('tweets/relevant_tweets.txt')
 df.columns = ['tweet']
@@ -10,6 +16,16 @@ query = "love food hurricane harvey texas houston flood road drink water bottle"
 cvec = CountVectorizer(vocabulary=query.split(' '))
 
 document_term_matrix = cvec.fit_transform(documents)
-document_term_matrix_as_array = document_term_matrix.toarray()
+# document_term_matrix = cvec.fit_transform(['love food is so good', 'hurricane is happing in texas'])
+document_term_matrix_as_array = np.array(document_term_matrix.toarray())
 print(document_term_matrix_as_array)
 
+
+good_documents = []
+for d in document_term_matrix_as_array:
+    if sum(d) >= 3:
+        good_documents.append(d)
+## seanbon
+# uniform_data = np.random.rand(10, 12)
+ax = sns.heatmap(good_documents, linewidth=0)
+plt.show()
