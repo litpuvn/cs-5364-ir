397fe70bf4c6f3930457d41f1f92c7a37106ddf5
Hoang Long <litpuvn@gmail.com>
2018-10-13 12:19:53 -0500

    add hierarchical clustering

---
+++ hierarchical_clustering.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
+
+np.random.seed(4711)  # for repeatability of this tutorial
+a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
+b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50,])
+X = np.concatenate((a, b),)
+print (X.shape)  # 150 samples with 2 dimensions
+plt.scatter(X[:,0], X[:,1])
+plt.show()
+
+
+
+# generate the linkage matrix
+Z = linkage(X, 'ward')
+Z = linkage(X, 'single')
+Z = linkage(X, 'complete')
+Z = linkage(X, 'average')
+
+
+
+# calculate full dendrogram
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('tf-idf vector index')
+plt.ylabel('distance')
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+plt.show()
