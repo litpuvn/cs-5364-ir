d47bf754f0e66972e947d325c55ff04248097276
Hoang Long <litpuvn@gmail.com>
2018-10-13 15:44:58 -0500

    add custom hierarchical clustering with cutoff line

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -3,34 +3,88 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 import numpy as np
 from scipy.cluster.hierarchy import fcluster
 
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
+
 np.random.seed(4711)  # for repeatability of this tutorial
 a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100,])
 b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50,])
 X = np.concatenate((a, b),)
-print (X.shape)  # 150 samples with 2 dimensions
-plt.scatter(X[:,0], X[:,1])
-plt.show()
+# print (X.shape)  # 150 samples with 2 dimensions
+# plt.scatter(X[:,0], X[:,1])
+# plt.show()
 
 
 
 # generate the linkage matrix
-Z = linkage(X, 'ward')
-Z = linkage(X, 'single')
+# Z = linkage(X, 'ward')
+# Z = linkage(X, 'single')
 Z = linkage(X, 'complete')
-Z = linkage(X, 'average')
+# Z = linkage(X, 'average')
 
-
+print(Z)
 
 # calculate full dendrogram
 plt.figure(figsize=(25, 10))
 plt.title('Hierarchical Clustering Dendrogram')
 plt.xlabel('tf-idf vector index')
 plt.ylabel('distance')
-dendrogram(
+# dendrogram(
+#     Z,
+#     leaf_rotation=90.,  # rotates the x axis labels
+#     leaf_font_size=8.,  # font size for the x axis labels
+# )
+# dendrogram(
+#     Z,
+#     truncate_mode='lastp',  # show only the last p merged clusters
+#     p=12,  # show only the last p merged clusters
+#     show_leaf_counts=False,  # otherwise numbers in brackets are counts
+#     leaf_rotation=90.,
+#     leaf_font_size=12.,
+#     show_contracted=True,  # to get a distribution impression in truncated branches
+# )
+# dendrogram(
+#     Z,
+#     truncate_mode='lastp',  # show only the last p merged clusters
+#     p=12,  # show only the last p merged clusters
+#     leaf_rotation=90.,
+#     leaf_font_size=12.,
+#     show_contracted=True,  # to get a distribution impression in truncated branches
+# )
+
+max_d = 10
+fancy_dendrogram(
     Z,
-    leaf_rotation=90.,  # rotates the x axis labels
-    leaf_font_size=8.,  # font size for the x axis labels
+    truncate_mode='lastp',  # show only the last p merged clusters
+    p=12,  # show only the last p merged clusters
+    leaf_rotation=90.,
+    leaf_font_size=12.,
+    show_contracted=True,  # to get a distribution impression in truncated branches
+    max_d=max_d,  # plot a horizontal cut-off line
+
 )
+
 plt.show()
 
 ####
