1bd3ee135244b35c52e0d89cfb4fb562e786fbeb
liang112233 <36117480+liang112233@users.noreply.github.com>
2018-10-18 17:33:26 -0500

    hw3
    hw3

--- kmean.py
+++ kmean.py
@@ -9,9 +9,10 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 import seaborn as sns
 import matplotlib.pyplot as plt
-#import matplotlib.pyplot as plt, mpld3
+import matplotlib.pyplot as plt, mpld3
 from matplotlib import style
 style.use('ggplot')
+from sklearn.decomposition import PCA
 
 class K_Means:
     def __init__(self, k=3, tolerance=0.0001, max_iterations=500):
@@ -31,15 +32,13 @@ class K_Means:
                 self.classes[i] = []
 
             # find the similarity distance between the point and cluster; choose the nearest centroid
-
             for features in data:
             #    print(features)
               #distances = [np.linalg.norm((features - self.centroids[centroid])) for centroid in self.centroids]
-              distances = [self.consine_similarity(features , self.centroids[centroid]) for centroid in self.centroids]
+              distances = [self.calculate_consine_similarity(features , self.centroids[centroid]) for centroid in self.centroids]
               print(distances)
               classification = distances.index(max(distances))
               print(classification)
-
               self.classes[classification].append(features)
               previous = dict(self.centroids)
             #print(previous)
@@ -59,32 +58,17 @@ class K_Means:
             if isOptimal:
                 break
 
-    def consine_similarity(self, vec1, vec2):
+    def calculate_consine_similarity(self, vec1, vec2):
        # form vector for similarity
-        sim = cosine_similarity(vec1, vec2)
-        sim1 = sim[0]
-        print(sim1)
-        return sim
+       sim = cosine_similarity([vec1], [vec2])
+       sim1 = sim[0]
+        #print(sim1)
+       return sim1[0]
 
-    # def consine_similarity(raw1, raw2):
-    #     attribute_list = []
-    #     raw1_attrs = raw1.split()
-    #     raw2_attrs = raw2.split()
-    #
-    #     # form attribute list
-    #     for t in (raw1_attrs + raw2_attrs):
-    #         if t not in attribute_list:
-    #             attribute_list.append(t)
-
-        # # form vector for similarity
-        # vec1 = create_binary_vector(raw1_attrs, attribute_list)
-        # vec2 = create_binary_vector(raw2_attrs, attribute_list)
-        #
-        # sim = cosine_similarity([vec1], [vec2])
-        # sim1 = sim[0]
-        # print(sim1)
-        # return sim1[0]
-
+    def pred(self, data):
+	    distances = [self.calculate_consine_similarity(data ,self.centroids[centroid]) for centroid in self.centroids]
+	    classification = distances.index(max(distances))
+	    return classification
 
 def main():
     df = pd.read_table('tweets/relevant_tweets.txt')
@@ -102,22 +86,46 @@ def main():
         if sum(d) >= 0.5:
             good_documents.append(d)
     #print(good_documents)
-    X = good_documents
-    km = K_Means(3)
-    km.fit(X)
+    X = np.array(good_documents)
+    # print(X)
+    # km = K_Means(3)  #
+    # km.fit(X)
+    # ###PCA
+    # from sklearn.decomposition import PCA
+    # pca = PCA(n_components=2).fit(X)
+    # pca_2d = pca.transform(X)
 
-    # # Plotting starts here
-    # colors = 10 * ["r", "g", "c", "b", "k"]
-    #
-    # for centroid in km.centroids:
-    #     plt.scatter(km.centroids[centroid][0], km.centroids[centroid][1], s=130, marker="x")
-    #
-    # for classification in km.classes:
-    #     color = colors[classification]
-    #     for features in km.classes[classification]:
-    #         plt.scatter(features[0], features[1], color=color, s=30)
-    #
-    # mpld3.show()
+    # Visualize the results on PCA-reduced data
+    reduced_data = PCA(n_components=2).fit_transform(X)
+    km = K_Means(3)  #
+    km.fit(reduced_data)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02  # point in the mesh [x_min, x_max]x[y_min, y_max].
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Obtain labels for each point in mesh. Use last trained model.
+    Z = km.pred(np.c_[xx.ravel(), yy.ravel()])
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(Z, interpolation='nearest',extent=(xx.min(), xx.max(), yy.min(), yy.max()),cmap=plt.cm.Paired,aspect='auto', origin='lower')
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+    # Plot the centroids as a white X
+    centroids = km.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],marker='x', s=169, linewidths=3,color='w', zorder=10)
+    plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n''Centroids are marked with white cross')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
      main()
