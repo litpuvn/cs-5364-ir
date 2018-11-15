ac0a13b2ad02e909fc474b82d283327ac49e7feb
Hoang Long <litpuvn@gmail.com>
2018-11-14 18:50:24 -0600

    update code

--- classifier/word_probability.py
+++ classifier/word_probability.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
+from pandas.core.series import Series
+
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
 
-df = pd.read_csv("train.csv", sep=",", header=0)
+df = pd.read_csv("hw_train.csv", sep="|", header=0)
 
 word_frequency = dict()
 class_frequency = dict()
@@ -12,10 +15,7 @@ doc_class_frequency = dict()
 for index, row in df.iterrows():
     tweet = row.iloc[1]
     label = row.iloc[2]
-
-    if "unknown" in label:
-        query_x = tweet
-        break
+    # counting documents and documents per class for class probability
     doc_count = doc_count + 1
     if label not in doc_class_frequency:
         doc_class_frequency[label] = 0
@@ -24,8 +24,10 @@ for index, row in df.iterrows():
     if label not in class_frequency:
         class_frequency[label] = dict()
         class_frequency[label + "-count"] = 0
-
+    # counting words per class for word probabilities
     current_class = class_frequency[label]
+    if type(tweet) is float:
+        print("test")
     words = tweet.split()
     for w in words:
 
@@ -40,7 +42,7 @@ for index, row in df.iterrows():
         word_frequency[w] = word_frequency[w] + 1
 
 voc_size = len(word_frequency)
-
+# print(voc_size)
 ## compute word probability ###
 # for label, word_class_frequency in class_frequency.items():
 #     if "count" in label:
@@ -61,13 +63,23 @@ voc_size = len(word_frequency)
 
 ## compute the query class ###
 
-df = pd.read_csv("test.csv", sep=",", header=0)
+# sorted_by_value = sorted(class_frequency["MUSIC"].items(), key=lambda kv: kv[1])
+# count = 0
+# s = [(k, class_frequency["FINANCE"][k]) for k in sorted(class_frequency["FINANCE"], key=class_frequency["FINANCE"].get, reverse=True)]
+# for k, v in s:
+#    print("key:", k, "; val:", v)
+#    count = count + 1
+#    if count > 9:
+#        break
+
+
+df = pd.read_csv("hw_test.csv", sep="|", header=0)
 y_true = []
 y_pred = []
 for _, row in df.iterrows():
     query_x = row.iloc[1]
-    label = row.iloc[2]
-    y_true.append(label)
+    query_label = row.iloc[2]
+    y_true.append(query_label)
     x_words = list(set(query_x.split()))
     max_prob = 0
     max_class = None
@@ -98,10 +110,15 @@ for _, row in df.iterrows():
 
     print()
     print("Query:", query_x)
-    print("predicted class:", max_class, "; prob:", max_prob)
+    print("predicted class:", max_class, "; prob:", max_prob, "; actual:", query_label)
     y_pred.append(max_class)
 
+df['predicted'] = Series(np.array(y_pred), index=df.index)
 
-precision, recall, f_score, _ = precision_recall_fscore_support(y_true=y_true, y_pred=y_pred)
+df.to_csv("predicted.csv", sep="|", index=False)
+
+precision, recall, f_score, _ = precision_recall_fscore_support(y_true=y_true, y_pred=y_pred,
+                                                                labels=['MUSIC', 'SPORT', 'FINANCE'], average="weighted")
 accuracy = accuracy_score(y_true=y_true, y_pred=y_pred)
 print("accuracy:", accuracy, "precision:", precision, "; recall:", recall, "; f-score:", f_score)
+
